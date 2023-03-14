73b4b563c45ca32af3c7296e300bc677f57ed993
AthomsLL <thomas.leblanc61@gmail.com>
2023-03-14 18:36:57 +0100

    Finalize RequestsReceived Component

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,14 +1,30 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas
     };
+
+    const response = await fetch(`https://finder-coach-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(coachData)
+    });
+
+    // const responseData = await response.json();
+
+    if(!response.ok) {
+      // error...
+    }
+
+
     
-    context.commit('registerCoach', coachData);
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
   }
 }
