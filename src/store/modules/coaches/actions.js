3233cde4ffce46098106a7ad0ab28f4a24bf33a3
AthomsLL <thomas.leblanc61@gmail.com>
2023-03-17 18:11:30 +0100

    Fix bug in Coaches store

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -2,6 +2,7 @@ export default {
   async registerCoach(context, data) {
     const userId = context.rootGetters.userId;
     const coachData = {
+      id: userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
@@ -9,7 +10,7 @@ export default {
       areas: data.areas
     };
 
-    const response = await fetch(`https://finder-coach-default-rtdb.europe-west1.firebasedatabase.app/coaches/${userId}.json`, {
+    const response = await fetch(`https://finder-coach-default-rtdb.europe-west1.firebasedatabase.app/coaches.json`, {
       method: 'PUT',
       body: JSON.stringify(coachData)
     });
@@ -23,7 +24,6 @@ export default {
 
     context.commit('registerCoach', {
       ...coachData,
-      id: userId
     });
   },
   async loadCoaches(context, payload) {
@@ -46,7 +46,7 @@ export default {
 
     for (const key in responseData) {
       const coach = {
-        id: key,
+        id: responseData[key].id,
         firstName: responseData[key].firstName,
         lastName: responseData[key].lastName,
         description: responseData[key].description,
